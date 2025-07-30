4ee0e059b7843d20f85779ccd98f977b5973b18e
samueleangel <samueleangel@gmail.com>
2025-07-30 22:53:59 +0200

    Added function to_str and converted the float successfully

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This script handles a function that converts a float
+into a string with the same value represented
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function will convert a float into a string,
+    with the same value represented, just changing it´s
+    data type.
+
+    Args:
+        n (float): Float data type
+
+    Returns:
+        n converted to string: "n"
+    """
+    return str(n)
