d74120a9bf175fd1ab9521ceeb03bec0a838241c
samueleangel <samueleangel@gmail.com>
2025-07-30 22:46:36 +0200

    Added floor function

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Script that holds a function that takes a float
+as argument and returns the floor of the float.
+"""
+
+
+def floor(n: float):
+    """
+    Function that returns the floor of a
+    float
+
+    Args:
+        n (float): Value of type float
+
+    Returns:
+        The floor of the float (int)
+    """
+    return int(n)
