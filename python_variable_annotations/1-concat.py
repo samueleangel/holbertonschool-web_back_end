533e18e837147ee22fbf79f74c3dab04ae467d3a
samueleangel <samueleangel@gmail.com>
2025-07-30 22:30:14 +0200

    Added concatenation function

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Script for a type-annotated function that concats two
+values of type string
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function to sum (concat) the variables str1
+    and str2.
+
+    Args:
+        str1 (string): First value type string
+        str2 (string): Second value type string
+
+    Returns:
+        str1 + str2 = Concatenated string
+    """
+    return str1 + str2
