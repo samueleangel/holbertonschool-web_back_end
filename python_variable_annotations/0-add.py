ec21cc63e5106f9b9164a4c5689065ea3a0c25a4
samueleangel <samueleangel@gmail.com>
2025-07-30 22:23:26 +0200

    Added function add with type annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Sum function
+
+    Args:
+        a (float): First float value
+        b (float): Second float value
+
+    Returns:
+        Sum of a + b, returns float
+    """
+    return a + b
