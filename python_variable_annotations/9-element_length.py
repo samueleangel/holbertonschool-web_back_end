8760fca344491b9802119874c3f1bc3fe6112a94
samueleangel <samueleangel@gmail.com>
2025-07-31 23:22:40 +0200

    Fix on pycodestyle

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -7,6 +7,7 @@ and its length.
 
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Takes an iterable of sequences (like lists or strings) and returns
@@ -16,6 +17,7 @@ def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
         lst (Iterable[Sequence]): An iterable containing sequence elements.
 
     Returns:
-        List[Tuple[Sequence, int]]: A list of tuples with each element and its length.
+        List[Tuple[Sequence, int]]: A list of tuples with each element and
+        its length.
     """
     return [(i, len(i)) for i in lst]
